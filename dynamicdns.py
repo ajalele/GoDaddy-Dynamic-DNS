9aff6801753f6c41f50befc38b3f46f8e3d9bdea
AJ <graciashusband@gmail.com>
2018-02-18 19:35:18 -0600

    Update dynamicdns.py

--- dynamicdns.py
+++ dynamicdns.py
@@ -20,21 +20,21 @@ def managedns(client, domain):  #  Gets your public IP address and compares it t
             dns_ip = client.get_records(domain, record_type='A')[0]['data']
             if  public_ip != dns_ip:
                 client.update_ip(public_ip, domains=[domain])
-                f.write('{} - DNS updated from {} to {}'.format(dateandtime(), dns_ip, public_ip,) + '\n')
+                f.write('{} - DNS updated from {} to {}'.format(dateandtime(), dns_ip, public_ip,) + '\r\n')
                 f.close()
             else:
-                f.write('{} - No changes needed\n'.format(dateandtime()))  # This is optional.  If you don't want this feedback feel free to remove it.
+                f.write('{} - No changes needed\r\n'.format(dateandtime()))  # This is optional.  If you don't want this feedback feel free to remove it.
                 f.close()
             time.sleep(60)  # This is the amount of time, in seconds, the program will wait before checking again.  Change this as you see fit.
         except (requests.ConnectionError, godaddypy.client.BadResponse) as e:
             if 'Response Data:' in str(e):
                 print(e.args[0]['message'])
-                f.write('{} - '.format(dateandtime()) + e.args[0]['message'] + '\n')
+                f.write('{} - '.format(dateandtime()) + e.args[0]['message'] + '\r\n')
                 f.close()
                 break
             else:
                 print('Cannot connect.  Trying agian in 10 seconds')
-                f.write('{} - Cannot Connect\n'.format(dateandtime()))
+                f.write('{} - Cannot Connect\r\n'.format(dateandtime()))
                 f.close()
                 time.sleep(10)  # This is the amount of time, in seconds, the program will wait before trying to connect again.  Change this as you see fit.
                 continue
