4a6c399194abd1f701a3727de7a752ababa91e67
AJ <graciashusband@gmail.com>
2018-02-21 18:03:10 -0600

    Update dynamicdns.py

--- dynamicdns.py
+++ dynamicdns.py
@@ -1,17 +1,17 @@
-import datetime, godaddypy, time, requests  # godaddpy was created by Julian Coy.  https://github.com/eXamadeus/godaddypy
+import datetime, godaddypy, requests, time   # godaddpy was created by Julian Coy.  https://github.com/eXamadeus/godaddypy
+from sys import argv
 
 
 def dateandtime():  # Returns the current date and time in desired format.
     return datetime.datetime.now().strftime('%D  %H:%M:%S')
 
 
-def godaddyapi():  # Returns godaddy API Client object.
-    a = godaddypy.Account(api_key='<your godaddy api key>', api_secret='<your godaddy api secret>')
-    c = godaddypy.Client(a)
-    return c
+def godaddyapi(key, secret):  # Returns godaddy API Client object.
+    a = godaddypy.Account(api_key=key, api_secret=secret)
+    return godaddypy.Client(a)
 
 
-def managedns(client, domain):  #  Gets your public IP address and compares it to your domain's A record IP address.  Changes A record to match if needed.
+def managedns(client, domain, repeatsec):  #  Gets your public IP address and compares it to your domain's A record IP address.  Changes A record to match if needed.
     while True:
         f = open('dynamicdns_stat.txt', 'a')
         try:
@@ -25,7 +25,9 @@ def managedns(client, domain):  #  Gets your public IP address and compares it t
             else:
                 f.write('{} - No changes needed\r\n'.format(dateandtime()))  # This is optional.  If you don't want this feedback feel free to remove it.
                 f.close()
-            time.sleep(60)  # This is the amount of time, in seconds, the program will wait before checking again.  Change this as you see fit.
+            if repeatsec == 0:
+                break
+            time.sleep(repeat)  # This is the amount of time, in seconds, the program will wait before checking again.  Change this as you see fit.
         except (requests.ConnectionError, godaddypy.client.BadResponse) as e:
             if 'Response Data:' in str(e):
                 print(e.args[0]['message'])
@@ -41,4 +43,7 @@ def managedns(client, domain):  #  Gets your public IP address and compares it t
         
         
 if __name__ == '__main__':
-    managedns(godaddyapi(), '<your domain>')
+    try:
+        managedns(godaddyapi(argv[2], argv[3]), argv[1], int(argv[4]))
+    except IndexError:
+        print('\nNot enough arguments given.\n\nPlease use the following format:\n\ndynamicdns.py <domain> <api key> <api secret> <how often (in seconds) you want to check IP addresses>\n')
